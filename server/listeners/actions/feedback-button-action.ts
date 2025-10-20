c61d57bd3104e9339637a0b947cec85282e5a296
malewis5 <matthew.a.lewis7@gmail.com>
2025-10-20 13:32:33 -0700

    add streaming + feedback

---
+++ server/listeners/actions/feedback-button-action.ts
@@ -0,0 +1,27 @@
+import type {
+  AllMiddlewareArgs,
+  BlockFeedbackButtonsAction,
+  SlackActionMiddlewareArgs,
+} from "@slack/bolt";
+
+export const feedbackButtonsCallback = async ({
+  action,
+  ack,
+  logger,
+  body,
+  client,
+}: AllMiddlewareArgs &
+  SlackActionMiddlewareArgs<BlockFeedbackButtonsAction>) => {
+  await ack();
+  // Send to your API or database
+  const { value } = action;
+  const [thread_ts, feedback_type] = value.split(":");
+  const user = body.user.id;
+  logger.info(`Feedback button action received: ${feedback_type}`);
+  await client.chat.postEphemeral({
+    channel: body.channel.id,
+    thread_ts,
+    user,
+    text: "Thank you for your feedback!",
+  });
+};
