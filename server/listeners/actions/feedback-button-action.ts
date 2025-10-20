d922a54766b3cfc83c64d1e074a34116dff603da
malewis5 <matthew.a.lewis7@gmail.com>
2025-10-20 13:38:09 -0700

    add error handling

--- server/listeners/actions/feedback-button-action.ts
+++ server/listeners/actions/feedback-button-action.ts
@@ -12,16 +12,20 @@ export const feedbackButtonsCallback = async ({
   client,
 }: AllMiddlewareArgs &
   SlackActionMiddlewareArgs<BlockFeedbackButtonsAction>) => {
-  await ack();
-  // Send to your API or database
-  const { value } = action;
-  const [thread_ts, feedback_type] = value.split(":");
-  const user = body.user.id;
-  logger.info(`Feedback button action received: ${feedback_type}`);
-  await client.chat.postEphemeral({
-    channel: body.channel.id,
-    thread_ts,
-    user,
-    text: "Thank you for your feedback!",
-  });
+  try {
+    await ack();
+    // Send to your API or database
+    const { value } = action;
+    const [thread_ts, feedback_type] = value.split(":");
+    const user = body.user.id;
+    logger.info(`Feedback button action received: ${feedback_type}`);
+    await client.chat.postEphemeral({
+      channel: body.channel.id,
+      thread_ts,
+      user,
+      text: "Thank you for your feedback!",
+    });
+  } catch (error) {
+    logger.error("Failed to send feedback:", error);
+  }
 };
