06082547f1c41e593a3c1f8daef9a287062689f7
malewis5 <matthew.a.lewis7@gmail.com>
2026-02-23 15:47:18 -0500

    fix database url

--- server/db/index.ts
+++ server/db/index.ts
@@ -2,6 +2,10 @@ import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 import * as schema from "./schema";
 
-const client = postgres(process.env.DATABASE_URL as string);
+if (!process.env.DATABASE_URL) {
+  throw new Error("DATABASE_URL is not set");
+}
+
+const client = postgres(process.env.DATABASE_URL);
 
 export const db = drizzle(client, { schema });
